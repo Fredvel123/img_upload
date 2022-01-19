b1b692943dfb2283a75530ec666e1590edc3de7c
Freddy <freddyvelarde59@gmail.com>
2022-01-19 13:24:08 -0400

    I added the data base atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI_DB = 'mongodb://0.0.0.0:27017/images_db';
+// const URI_DB = 'mongodb://0.0.0.0:27017/images_db';
+const URI_DB = `mongodb+srv://${process.env.USER_ATLAS}:${process.env.PASSWD_ATLAS}@cluster0.iyu84.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const config = {
   useNewUrlParser: true,
   useUnifiedTopology: true
